637a2cce8bf418aefa9399b80d02560e60eaa655
Davison <d.p.davison@utwente.nl>
2025-01-10 22:20:38 +0100

    now iterates over all BT CSV files for each smartwatch to compute closeness score and exports to csv

--- parse_ble_devices.py
+++ parse_ble_devices.py
@@ -2,7 +2,19 @@ import csv
 from datetime import datetime
 import os
 
-def read_csv(file_path):
+DATA_DIR = 'data'
+OUTPUT_FILE='closeness_scores.csv'
+SMARTWATCH_BT_WHITELIST = ['54:08:3B:C4:FC:64','51:62:7F:43:DA:1A','3E:EE:A1:BF:47:DE','29:5A:3F:E1:97:2B','74:40:BB:C2:DF:46','28:D3:3E:0A:80:93','4E:8A:2B:9E:F9:42']
+
+def get_all_csv_files(directory, filename_pattern):
+    csv_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.csv') and filename_pattern in file:
+                csv_files.append(os.path.join(root, file))
+    return csv_files
+
+def read_bluetooth_csv(file_path):
     data = []
     with open(file_path, mode='r') as file:
         csv_reader = csv.reader(file, delimiter=',')
@@ -11,6 +23,16 @@ def read_csv(file_path):
             timestamp = datetime.fromtimestamp(int(timestamp) / 1000.0)
             data.append({'timestamp': timestamp,'devices': devices})	
     return data
+
+def parse_bluetooth_data(data):
+    results = []
+    for row in data:
+        devices = parse_bluetooth_devices(row)
+        results.append({
+            'timestamp': row['timestamp'],
+            'devices': devices
+        })
+    return results
 
 def parse_bluetooth_devices(row):
     devices = {}
@@ -22,31 +44,11 @@ def parse_bluetooth_devices(row):
                     devices[device_info[0]] = 128-int(device_info[1]) #invert the signal strength so that higher number == better signal
     return devices
 
-def parse_data(data):
-    results = []
-    for row in data:
-        devices = parse_bluetooth_devices(row)
-        results.append({
-            'timestamp': row['timestamp'],
-            'devices': devices
-        })
-    return results
-
-def get_all_csv_files(directory):
-    csv_files = []
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.endswith('.csv'):
-                subdirectory = os.path.relpath(root, directory)
-                csv_files.append((os.path.join(root, file), subdirectory))
-    return csv_files
-
 def calc_cumulative_closeness_score(data):
-    smartwatch_bt_whitelist = ['51:62:7F:43:DA:1A','3E:EE:A1:BF:47:DE','29:5A:3F:E1:97:2B']
     closeness_scores = {}
     for row in data:
         for device, signal_strength in row['devices'].items():
-            if device not in smartwatch_bt_whitelist:
+            if device not in SMARTWATCH_BT_WHITELIST:
                 continue
             
             if device in closeness_scores:
@@ -63,15 +65,35 @@ def calc_cumulative_closeness_score(data):
                 )
     return closeness_scores
 
+def export_scores_to_csv(scores, output_file):
+    with open(output_file, mode='w', newline='') as file:
+        print(f"Storing data for watch ID {id}: Device {device} was in range for {scores['TIR']} measurements with a total closeness score of {scores['RSSI']}")
+        writer = csv.writer(file)
+        writer.writerow(['device', 'TIR', 'RSSI'])
+        for device, scores in scores.items():
+            writer.writerow([device, scores['TIR'], scores['RSSI']])
 
 def main():
-    
-    file_path = 'data\\0MVY\\000_BT_efa4912b299d4a4e_1736261732558.csv'
-    data = read_csv(file_path)
-    results = parse_data(data)
-    closeness_scores = calc_cumulative_closeness_score(results)
-    for device,scores in closeness_scores.items():
-        print(f"Device {device} was in range for {scores['TIR']} measurements with a total closeness score of {scores['RSSI']}")
+    closeness_scores = {}
+    smartwatch_ids = next(os.walk(DATA_DIR))[1]
+    for id in smartwatch_ids:
+        print(f"Processing data for smartwatch {id}")
+        bluetooth_log_files = get_all_csv_files(DATA_DIR+'\\'+id, '_BT_')
+        bluetooth_data = []
+        for file_path in bluetooth_log_files:
+            print(f"Processing {file_path}")
+            bluetooth_data += parse_bluetooth_data(read_bluetooth_csv(file_path))
+        closeness_scores[id] = calc_cumulative_closeness_score(bluetooth_data)
+
+    with open(OUTPUT_FILE, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['watch_id','bt_device', 'TIR', 'RSSI'])
+        for id,scores in closeness_scores.items():
+            for device, scores in scores.items():
+                print(f"Storing data for watch ID {id}: Device {device} was in range for {scores['TIR']} measurements with a total closeness score of {scores['RSSI']}")
+                writer.writerow([id, device, scores['TIR'], scores['RSSI']])
+
+
 
 if __name__ == "__main__":
     main()
